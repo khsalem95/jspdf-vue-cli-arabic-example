16f07a44131c6018ccccf07499533ddb6cbcddd3
Khaled AlHaj Salem <khaled.salem.it@gmail.com>
2023-07-05 09:46:13 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+//import AmiriRegular from './assets/Amiri-Regular.ttf'
+//import  AmiriFont  from './amiri_font.js'
+import { jsPDF } from "jspdf";
+
+
+
+// Default export is a4 paper, portrait, using millimeters for units
+const pdf = new jsPDF();
+// pdf.addFileToVFS('Amiri-Regular.ttf', AmiriFont.font);
+// pdf.addFont('Amiri-Regular.ttf', 'Amiri-Regular', 'normal');
+// pdf.setFont("Amiri-Regular");
+//
+//
+// //doc.text("السلام عليكم", 10, 10,{lang: "ar"});
+//
+// pdf.html(document.getElementById('html-print'), {
+//     callback: function (pdf) {
+//         window.open(pdf.output('bloburl'));
+//         //pdf.save('a4.pdf');
+//     },
+//     lang: "ar"
+// });
+
+const AmiriRegular = pdf.loadFile("https://rawcdn.githack.com/MrRio/jsPDF/cbc85b4bb66d5c1a2d50d199e8bf33836f2f12d7/test/reference/Amiri-Regular.ttf", true);
+pdf.addFileToVFS("Amiri-Regular.ttf", AmiriRegular);
+
+pdf.addFont("Amiri-Regular.ttf", "Amiri", "normal");
+pdf.html(document.getElementById('html-print'), {
+    callback: function (pdf) {
+        window.open(pdf.output('bloburl'));
+        /*var iframe = document.createElement('iframe');
+        iframe.setAttribute('style', 'position:absolute;top:0;right:0;height:100%; width:600px');
+        document.body.appendChild(iframe);
+        iframe.src = pdf.output('datauristring');*/
+    }
+});
+
+//doc.save("a4.pdf");
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello" id="html-print">
+
+    <p class="amiri">السلام عليكم </p>
+    <p class="amiri">كيف حالكم </p>
+      <table class="table mb-0 table-bordered amiri">
+          <thead class="card-header">
+          <tr>
+              <td class="col-3 amiri"><strong>Nature of goods or
+                  services
+                  تفاصیل السلع أو الخدمات</strong></td>
+              <td class="col-2 text-center amiri"><strong>Unit price</strong><br><strong>سعر الوحدة</strong></td>
+              <td class="col-1 text-center amiri"><strong>Quantity</strong><br><strong>الكمية</strong></td>
+              <td class="col-3 text-end amiri"><strong>Item Subtotal (Including VAT)</strong><br><strong>المجموع (شامل ضریبة القیمة المضافة)</strong></td>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+              <td class="col-3 amiri">باقة رصيد تقارير - 200 تقرير تفصيلي</td>
+              <td class="col-2 text-center amiri">6,000.00 SAR</td>
+              <td class="col-1 text-center amiri">1</td>
+              <td class="col-3 text-end amiri">6,900.00 SAR</td>
+          </tr>
+          </tbody>
+          <tfoot class="card-footer amiri">
+          <tr>
+              <td colspan="3" class="text-end">
+                  <sapn>الإجمالي الخاضع للضریبة (غیر شامل ضریبة القیمة
+                      المضافة):</sapn> <br> <sapn>Total Taxable Amount (Excluding VAT)</sapn></td>
+              <td class="text-end">6,000.00 SAR</td>
+          </tr>
+          <tr>
+              <td colspan="3" class="text-end"><sapn>مجموع ضریبة القیمة المضافة:</sapn><br><sapn>Total VAT</sapn></td>
+              <td class="text-end">900.00 SAR</td>
+          </tr>
+          <tr>
+              <td colspan="3" class="text-end border-bottom-0"><sapn>إجمالي المبلغ المستحق:</sapn><br><sapn>Total Amount Due</sapn></td>
+              <td class="text-end border-bottom-0">6,900.00 SAR</td>
+          </tr>
+          </tfoot>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.amiri {
+    font-family: "Amiri";
+}
+</style>
